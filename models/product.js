0768d2120365c207bfe0b7aeb4b7b8c95cc16ecc
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-02-10 21:15:59 +0100

    used callback to solve products undefined bug

--- models/product.js
+++ models/product.js
@@ -1,6 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
+const filePath = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = callback => {
+  fs.readFile(filePath, (err, fileContent) => {
+    if (err) {
+      callback([]);
+    } else {
+      callback(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   // Define the shape of a product
   constructor(title) {
@@ -9,38 +25,19 @@ module.exports = class Product {
 
   save = () => {
     // save product to a file
-    const filePath = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    // store a product to this file
-    // get the existing product from the file
-    fs.readFile(filePath, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       // Save the data back into the file
       fs.writeFile(filePath, JSON.stringify(products), err => {
         if (err) console.log(err);
       });
     });
+    // store a product to this file
+    // get the existing product from the file
   };
 
   // retrieve all products from our array
   static fetchAll = callback => {
-    const filePath = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(filePath, (err, fileContent) => {
-      if (err) {
-        callback([]);
-      }
-      callback(JSON.parse(fileContent));
-    });
+    getProductsFromFile(callback);
   };
 };
