23ad92f8c55a46f9dfef5a93eef6511bc92da60f
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-02-10 12:06:17 +0100

    Added views to display on each route

--- express_app.js
+++ express_app.js
@@ -1,18 +1,24 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// use allows us to add a new middleware function.
-app.use((req, res, next) => {
-  // This function we pass to 'use' will be executed for every incoming request.
-  console.log('In the middleware');
-  // next function allows the request to be tunneled down to the next middlware function in line.
-  next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Hello from Express!</h1>');
+// Add middleware before our route handling middlewares because the parsing of the body should be done no matter where the request of the body ends up.
+// Parse the incoming request body in our express app
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Only routes starting with /admin will go to the adminRoutes file
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// This is a catch all middleware that returns 404
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000, () => {
---
+++ routes/admin.js
@@ -0,0 +1,20 @@
+const path = require('path');
+// This route handles the creation of products
+const express = require('express');
+
+// router is like a mini express app that we can connect to our express app
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+});
+
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  // Redirect and log the incoming data to the console
+  res.redirect('/');
+});
+
+module.exports = router;
