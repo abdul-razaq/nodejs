1917204c9f217933161b28e831542c9599488ca0
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-02-10 21:04:56 +0100

    used callback to solve products undefined bug

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
   // Define the shape of a product
@@ -7,11 +8,39 @@ module.exports = class Product {
   }
 
   save = () => {
-    products.push(this);
+    // save product to a file
+    const filePath = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    // store a product to this file
+    // get the existing product from the file
+    fs.readFile(filePath, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      // Save the data back into the file
+      fs.writeFile(filePath, JSON.stringify(products), err => {
+        if (err) console.log(err);
+      });
+    });
   };
 
   // retrieve all products from our array
-  static fetchAll = () => {
-    return products
+  static fetchAll = callback => {
+    const filePath = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(filePath, (err, fileContent) => {
+      if (err) {
+        callback([]);
+      }
+      callback(JSON.parse(fileContent));
+    });
   };
 };
