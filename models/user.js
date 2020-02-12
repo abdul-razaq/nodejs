1b79e9fc26198cb08f65ff1628311367b009152a
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-02-12 21:02:51 +0100

    Added User model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../utils/database');
+
+// Define the structure of the user
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
